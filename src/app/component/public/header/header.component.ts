e21c2e6112df1d222e5433883d48ab4db165e672
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-01-21 13:11:01 +0700

    đã sữa xong log in, log out, sign up

--- src/app/component/public/header/header.component.ts
+++ src/app/component/public/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Categories} from '../../../model/categories.model';
+import {Promotion} from '../../../model/promotion.model';
+import {TokenStorageService} from '../../../user/_services/token-storage.service';
+import {CategoriesService} from '../../../service/categories.service';
+import {PromotionService} from '../../../service/promotion.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  private roles: string[];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username: string;
+  isShow = false;
+  categoryList: Categories[];
+  promotionList: Promotion[];
 
-  ngOnInit() {
+  constructor(private tokenStorageService: TokenStorageService,
+              private categoriesService: CategoriesService,
+              private promotionService: PromotionService) {
   }
 
+  ngOnInit() {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+    this.categoriesService.getCategoriesList().subscribe(next => {
+      this.categoryList = next;
+    }, error => (console.log(error)));
+    this.promotionService.getPromotionList().subscribe(next => (this.promotionList = next), error => (console.log(error)));
+  }
+
+  logout() {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
