13a5fbb51d81824536d6b8e9a3dcad1cf378866c
BachBeu <katobmt107@gmail.com>
2020-01-31 05:22:15 +0700

    fix lỗi không add aboutus và blog

--- src/app/component/public/blog/blog.component.ts
+++ src/app/component/public/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -11,10 +12,41 @@ export class BlogComponent implements OnInit {
 
   ngOnInit() {
     $(document).ready(function() {
-      $("a").click(function(){
-        $("#view_more").toggle("slow");
-      })
-    })
+      // Configure/customize these variables.
+      var showChar = 147;  // How many characters are shown by default
+      var ellipsestext = "";
+      var moretext = "Read More";
+      var lesstext = "Read Less";
+
+
+      $('.more').each(function() {
+        var content = $(this).html();
+
+        if(content.length > showChar) {
+
+          var c = content.substr(0, showChar);
+          var h = content.substr(showChar, content.length - showChar);
+
+          var html = c + '<span class="moreellipses">' + ellipsestext  + '</span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">' + moretext + '</a></span>';
+
+          $(this).html(html);
+        }
+
+      });
+
+      $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+          $(this).removeClass("less");
+          $(this).html(moretext);
+        } else {
+          $(this).addClass("less");
+          $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+      });
+    });
   }
 
 }
