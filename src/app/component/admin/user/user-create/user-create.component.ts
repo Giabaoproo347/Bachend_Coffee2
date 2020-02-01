5c2b2ea6f565f37a325ba0e6df823d6a07a0d778
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-02-01 17:07:37 +0700

    đã xong user create

--- src/app/component/admin/user/user-create/user-create.component.ts
+++ src/app/component/admin/user/user-create/user-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Categories} from '../../../../model/categories.model';
+import {CategoriesService} from '../../../../service/categories.service';
+import {Router} from '@angular/router';
+import {User} from '../../../../model/user.model';
+import {UserService} from '../../../../user/_services/user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  user: User;
+  message = false;
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.userForm = this.fb.group({
+      id: '',
+      name: ['', [Validators.required, Validators.minLength(1)]],
+      phone: ['', [Validators.required, Validators.minLength(1)]],
+      email: ['', [Validators.required, Validators.minLength(1)]],
+      address: ['', [Validators.required, Validators.minLength(1)]],
+    });
   }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const {value} = this.userForm;
+      this.user = value;
+      this.userService.createUser(this.user).subscribe(
+        next => {
+          console.log(next);
+          this.message = true;
+          this.ngOnInit();
+        }
+      );
+    } else {
+      console.log('error');
+    }
+  }
+
 
 }
