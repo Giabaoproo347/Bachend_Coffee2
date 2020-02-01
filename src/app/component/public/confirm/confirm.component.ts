4c7f56c3fedaf762f24a469d7076c0f8f2edb8e8
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-02-01 15:15:02 +0700

    đang làm payment with login

--- src/app/component/public/confirm/confirm.component.ts
+++ src/app/component/public/confirm/confirm.component.ts
@@ -9,6 +9,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {TokenStorageService} from '../../../user/_services/token-storage.service';
 import {PaymentService} from '../../../service/payment.service';
 import {Payment} from '../../../model/payment.model';
+import {AppComponent} from '../../../app.component';
+import {User} from '../../../model/user.model';
 
 interface ICartItemWithProduct extends CartItem {
   product: Product;
@@ -25,10 +27,12 @@ export class ConfirmComponent implements OnInit {
   public itemCount: number;
   public isSuccess = false;
   paymentForm: FormGroup;
+  userForm: FormGroup;
   isLoggedIn = false;
   payment: Payment;
   method = ['Ship cod', 'Ví Momo', 'Vietcombank', 'Zalo Pay', 'Viettel Pay', 'VNQR pay'];
   currentDate = new Date();
+  currentUser: User | any;
 
   private products: Product[];
   private cartSubscription: Subscription;
@@ -38,28 +42,29 @@ export class ConfirmComponent implements OnInit {
                      private productsService: ProductService,
                      private tokenStorageService: TokenStorageService,
                      private paymentService: PaymentService,
-                     private fb: FormBuilder
+                     private fb: FormBuilder,
+                     private app: AppComponent
   ) {
   }
 
   public ngOnInit(): void {
-    if (!this.tokenStorageService.getToken()) {
-      this.cart = this.shoppingCartService.get();
-      this.cartSubscription = this.cart.subscribe((cart) => {
-        this.itemCount = cart.items.map((x) => x.quantity).reduce((p, n) => p + n, 0);
-        this.productsService.getProductList().subscribe((products) => {
-          this.products = products;
-          this.cartItems = cart.items
-            .map((item) => {
-              const product = this.products.find((p) => p.id === item.productId);
-              return {
-                ...item,
-                product,
-                totalCost: product.price * item.quantity
-              };
-            });
-        });
+    this.cart = this.shoppingCartService.get();
+    this.cartSubscription = this.cart.subscribe((cart) => {
+      this.itemCount = cart.items.map((x) => x.quantity).reduce((p, n) => p + n, 0);
+      this.productsService.getProductList().subscribe((products) => {
+        this.products = products;
+        this.cartItems = cart.items
+          .map((item) => {
+            const product = this.products.find((p) => p.id === item.productId);
+            return {
+              ...item,
+              product,
+              totalCost: product.price * item.quantity
+            };
+          });
       });
+    });
+    if (!this.tokenStorageService.getToken()) {
       this.paymentForm = this.fb.group({
         id: '',
         name: ['', [Validators.required, Validators.minLength(1)]],
@@ -72,6 +77,23 @@ export class ConfirmComponent implements OnInit {
         date: this.currentDate,
         status: ['Đang chờ xử lý']
       });
+    } else {
+      this.isLoggedIn = true;
+      this.currentUser = this.tokenStorageService.getUser();
+      this.paymentForm = this.fb.group({
+        id: [''],
+        name: ['', [Validators.required, Validators.minLength(1)]],
+        address: this.currentUser.address,
+        phone: this.currentUser.phone,
+        email: this.currentUser.email,
+        total: [''],
+        description: [''],
+        method: ['Ship cod'],
+        date: this.currentDate,
+        status: ['Đang chờ xử lý']
+      });
+
+
     }
   }
 
