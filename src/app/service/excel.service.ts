5bfc750161af3a2a6f51c5e47f3ae4f070c87c0f
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-02-03 17:06:56 +0700

    sủa private thành public trong excel.service.ts

--- src/app/service/excel.service.ts
+++ src/app/service/excel.service.ts
@@ -20,7 +20,7 @@ export class ExcelService {
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
 
-  private saveAsExcelFile(buffer: any, fileName: string): void {
+  public saveAsExcelFile(buffer: any, fileName: string): void {
     const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
     FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
