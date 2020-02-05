2e72227d4e4502db2bf871d22fef19d0e9fb89e7
BachBeu <katobmt107@gmail.com>
2020-02-05 16:13:09 +0700

    bach them comment

--- src/app/user/_services/token-storage.service.ts
+++ src/app/user/_services/token-storage.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 
 const TOKEN_KEY = 'auth-token';
 const USER_KEY = 'auth-user';
-
+const ID_KEY = 'auth-userId';
+const USERNAME_KEY = 'auth-username';
+const EMAIL_KEY = 'Email';
+const PHONE_KEY = 'Phone';
+const ADDRESS_KEY = 'Address';
+const PASSWORD_KEY = 'Password';
 @Injectable({
   providedIn: 'root'
 })
@@ -32,5 +37,41 @@ export class TokenStorageService {
     return JSON.parse(sessionStorage.getItem(USER_KEY));
   }
 
+  public getPhone(): string {
+    return sessionStorage.getItem(PHONE_KEY);
+  }
+
+  public getAddress(): string {
+    return sessionStorage.getItem(ADDRESS_KEY);
+  }
+
+  public getPassword(): string {
+    return sessionStorage.getItem(PASSWORD_KEY);
+  }
+
+  public saveEmail(email: string) {
+    window.sessionStorage.removeItem(EMAIL_KEY);
+    window.sessionStorage.setItem(EMAIL_KEY, email);
+  }
+
+  public getEmail(): string {
+    return sessionStorage.getItem(EMAIL_KEY);
+  }
+  public saveUsername(username: string) {
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, username);
+  }
+
+  public getUsername(): string {
+    return sessionStorage.getItem(USERNAME_KEY);
+  }
+  public getUserId(): string {
+    return sessionStorage.getItem(ID_KEY);
+  }
+
+  public saveUserId(userId: string) {
+    window.sessionStorage.removeItem(ID_KEY);
+    window.sessionStorage.setItem(ID_KEY, userId);
+  }
 }
 
